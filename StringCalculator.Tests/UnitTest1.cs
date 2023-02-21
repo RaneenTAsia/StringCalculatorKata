4cb3e622085f3eda8c2ce00d255d64ee3963a415
Raneen Asia <Raneenasia101@gmail.com>
2023-02-21 12:50:10 +0200

    Add project setup files

---
+++ StringCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StringCalculator.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,5 @@
+﻿namespace StringCalculator;
+public class Class1
+{
+
+}
